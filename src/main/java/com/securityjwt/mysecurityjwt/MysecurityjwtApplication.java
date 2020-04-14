4d796a40bb3f54c9d343d2f47e174080ba329ba6
ghemulyanto <trimulyanto.tm@gmail.com>
2020-04-14 13:50:38 +0700

    First Commit

---
+++ src/main/java/com/securityjwt/mysecurityjwt/MysecurityjwtApplication.java
@@ -0,0 +1,13 @@
+package com.securityjwt.mysecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecurityjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecurityjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securityjwt/mysecurityjwt/MysecurityjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityjwt.mysecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysecurityjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
